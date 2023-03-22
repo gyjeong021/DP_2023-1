c17a68c2d1d65174b77139b44211d1456076fa17
gyjeong021 <gyjeong021@gmail.com>
2023-03-22 20:07:53 +0900

    3월14일 수업내용

--- src/ch01/practice/BookShelf.java
+++ src/ch01/practice/BookShelf.java
@@ -1,8 +1,8 @@
 package ch01.practice;
 
-public class BookShelf {
+public class BookShelf implements Iterable<Book> {
     private Book[] books; // 배열 선언
-    private int last; // 마지막에 꽂힌 위치
+    private int last; // 마지막에 꽂힌 위치, 책 갯수
 
     public BookShelf(int maxsize) {
         // 배열 생성
@@ -18,5 +18,16 @@ public class BookShelf {
     // 책 가져오는 메소드
     public Book getBookAt(int index) {
         return books[index];
+    }
+
+    // 책 갯수를 반환하는 메소드
+    public int getLegth() {
+        return last;
+    }
+
+    // 자신의 iterator를 반환하는 메소드
+    @Override
+    public BookShelfIterator iterator() {
+        return new BookShelfIterator(this);
     }
 }
